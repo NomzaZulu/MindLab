8bcb354c1c219956e63787c30085dbddbd974f8b
NomzaZulu <steel.backspin501@slmails.com>
2026-09-06 17:52:55 +0530

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/health")
+def health():
+    return jsonify({
+        "success": True,
+        "message": "MindLab backend is running"
+    })
+
+
+@app.route("/api")
+def api_home():
+    return jsonify({
+        "success": True,
+        "message": "MindLab API"
+    })
